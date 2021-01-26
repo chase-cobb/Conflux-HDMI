b5cd2ae758097c882b20f5198912f37fc5c7790d
Chase Cobb <chase@8bitroots.com>
2021-01-26 07:44:38 -0600

    Updated docs

--- Docs/html/search/functions_4.js
+++ Docs/html/search/functions_4.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['updateconfigvalues_75',['UpdateConfigValues',['../class_conflux_1_1_hdmi_interface.html#a96c5d0e291b1fa4ca31854527ba55349',1,'Conflux::HdmiInterface::UpdateConfigValues()'],['../class_conflux_1_1_xbox_h_d_m_i_1_1_xbox_hdmi.html#ab9da3d657fc416b7eb19f252df7397e6',1,'Conflux::XboxHDMI::XboxHdmi::UpdateConfigValues()']]],
   ['updatefeatureconfig_76',['UpdateFeatureConfig',['../class_conflux_1_1_hdmi_tools.html#a82d97492a55c51b8c54c57b101da68e4',1,'Conflux::HdmiTools']]],
-  ['updatefirmware_77',['UpdateFirmware',['../class_conflux_1_1_hdmi_interface.html#a2c1aea6b6098efcc6a034251eb427f00',1,'Conflux::HdmiInterface::UpdateFirmware()'],['../class_conflux_1_1_xbox_h_d_m_i_1_1_xbox_hdmi.html#af6a9d0171891dc0b1462718df980a834',1,'Conflux::XboxHDMI::XboxHdmi::UpdateFirmware()'],['../class_conflux_1_1_hdmi_tools.html#ac71a1ba6798e61dae0ed5da70466781d',1,'Conflux::HdmiTools::UpdateFirmware()']]]
+  ['updatefirmware_77',['UpdateFirmware',['../class_conflux_1_1_hdmi_interface.html#a4c4b72786018fb1329d81762376ea10e',1,'Conflux::HdmiInterface::UpdateFirmware()'],['../class_conflux_1_1_xbox_h_d_m_i_1_1_xbox_hdmi.html#a363d3a182bf3d400b890366fc98251da',1,'Conflux::XboxHDMI::XboxHdmi::UpdateFirmware()'],['../class_conflux_1_1_hdmi_tools.html#a4ce3a945d139b9202b4a10c185012aef',1,'Conflux::HdmiTools::UpdateFirmware()']]]
 ];
